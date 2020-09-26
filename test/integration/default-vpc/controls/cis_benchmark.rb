968e9e7b1cfd8a6da34137b2188f0ddb3b77c835
Brett Curtis <brett@lzy.sh>
2020-09-25 22:53:31 -0400

    Initial commit

--- test/integration/default-vpc/controls/cis_benchmark.rb
+++ test/integration/default-vpc/controls/cis_benchmark.rb
@@ -1,5 +1,5 @@
 require_controls 'inspec-gcp-cis-benchmark' do
-  comtrol 'cis-gcp-3.2-networking'
+  control 'cis-gcp-3.2-networking'
   control 'cis-gcp-3.6-networking'
   control 'cis-gcp-3.7-networking'
   control 'cis-gcp-3.8-networking'
