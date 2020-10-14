e421d47ec8d58e1226c0a0377ec6522a9d94f654
Brett Curtis <brett@lzy.sh>
2020-10-13 20:59:45 -0400

    Use IAP for TCP forwarding  Fixes #1

--- test/fixtures/shared/outputs.tf
+++ test/fixtures/shared/outputs.tf
@@ -1,7 +1,3 @@
-output "vpc_self_link" {
-  value = module.test.vpc_self_link
-}
-
 output "network_name" {
   value = module.test.network_name
 }
@@ -12,4 +8,8 @@ output "project_id" {
 
 output "subnet_ips" {
   value = module.test.subnet_ips
+}
+
+output "vpc_self_link" {
+  value = module.test.vpc_self_link
 }
