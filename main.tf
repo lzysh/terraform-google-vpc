5d18523085e1b13f14e7995ac080e6f4eed450a2
Brett Curtis <brett@lzy.sh>
2020-10-13 21:09:13 -0400

    Use IAP for TCP forwarding (#2)
    Fixes #1

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
+# Project Service Resource
+# https://www.terraform.io/docs/providers/google/r/google_project_service.html
+
 resource "google_project_service" "this" {
+  count = length(var.services)
+
   project = var.project_id
-  count   = length(var.services)
   service = element(var.services, count.index)
 
   disable_on_destroy = false
 }
+
+# Compute Network Resource
+# https://www.terraform.io/docs/providers/google/r/compute_network.html
 
 resource "google_compute_network" "this" {
   project                 = var.project_id
@@ -15,24 +22,24 @@ resource "google_compute_network" "this" {
   depends_on = [google_project_service.this]
 }
 
+# Compute Subnetwork Resource
+# https://www.terraform.io/docs/providers/google/r/compute_subnetwork.html
+
 resource "google_compute_subnetwork" "this" {
+  provider = google-beta
+
+  count = length(var.subnets)
+
   project       = var.project_id
-  count         = length(var.subnets)
   name          = var.subnets[count.index]["subnet_name"]
   ip_cidr_range = var.subnets[count.index]["subnet_range"]
   region        = var.subnets[count.index]["subnet_region"]
   network       = google_compute_network.this.self_link
-  dynamic "log_config" {
-    for_each = lookup(var.subnets[count.index], "subnet_flow_logs", false) ? [{
-      aggregation_interval = lookup(var.subnets[count.index], "subnet_flow_logs_interval", "INTERVAL_5_SEC")
-      flow_sampling        = lookup(var.subnets[count.index], "subnet_flow_logs_sampling", "0.5")
-      metadata             = lookup(var.subnets[count.index], "subnet_flow_logs_metadata", "INCLUDE_ALL_METADATA")
-    }] : []
-    content {
-      aggregation_interval = log_config.value.aggregation_interval
-      flow_sampling        = log_config.value.flow_sampling
-      metadata             = log_config.value.metadata
-    }
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
   }
 
   secondary_ip_range = [
@@ -46,19 +53,32 @@ resource "google_compute_subnetwork" "this" {
   var.secondary_ranges[var.subnets[count.index]["subnet_name"]][i]]
 }
 
-resource "google_compute_firewall" "fw_allow_health_check" {
-  project = var.project_id
-  name    = "${google_compute_network.this.name}-allow-health-check"
-  network = google_compute_network.this.name
+# Firewall Resource
+# https://www.terraform.io/docs/providers/google/r/compute_firewall.html
 
-  allow {
-    protocol = "tcp"
+resource "google_compute_firewall" "this" {
+  count = length(var.firewall_rules)
+
+  project       = var.project_id
+  name          = "${google_compute_network.this.name}-${var.firewall_rules[count.index]["name"]}"
+  network       = google_compute_network.this.name
+  source_ranges = var.firewall_rules[count.index]["source_ranges"]
+
+  dynamic "allow" {
+    for_each = [lookup(var.firewall_rules[count.index], "allow")]
+    content {
+      protocol = allow.value.protocol
+      ports    = lookup(allow.value, "ports", null)
+    }
   }
 
-  source_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
 }
 
+# Compute Shared VPC Host Resource
+# https://www.terraform.io/docs/providers/google/r/compute_shared_vpc_host_project.html
+
 resource "google_compute_shared_vpc_host_project" "this" {
-  count   = var.shared_vpc ? 1 : 0
+  count = var.shared_vpc ? 1 : 0
+
   project = var.project_id
 }
--- test/fixtures/shared/variables.tf
+++ test/fixtures/shared/variables.tf
@@ -1,9 +1,11 @@
 variable "cidr_range" {
   type    = string
+
   default = "172.23.21.0/24"
 }
 
 variable "secondary_ranges" {
   type    = map(list(object({ range_name = string, ip_cidr_range = string })))
+
   default = {}
 }
